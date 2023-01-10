800f1798a2fc9a6afb9fc2a3094faea47c14eb78
shalakasp@gmail.com <shalakasp@gmail.com>
2023-01-10 18:45:09 +0000

    circuits, races, mount adls

---
+++ ingest_circuits_file.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###Ingest circuits.csv file
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[StructField("circuitId",IntegerType(), False),
+                                     StructField("circuitRef",StringType(), True),
+                                     StructField("name",StringType(), True),
+                                     StructField("location",StringType(),True),
+                                     StructField("country", StringType(), True),
+                                     StructField("lat", DoubleType(), True),
+                                     StructField("lng", DoubleType(), True),
+                                     StructField("alt", IntegerType(), True),
+                                     StructField("url", StringType(), True)
+                                    ])
+
+# COMMAND ----------
+
+circuits_df = spark.read \
+            .option("header", True) \
+            .schema(circuits_schema) \
+            .csv("/mnt/group5dxc/bronze/circuits.csv")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/group5dxc/bronze/circuits.cs
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select(col("circuitId"), col("circuitRef"), col("name"), col("location"), col("country"), col("lat"), col("lng"), col("alt"))
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId", "circuit_id") \
+.withColumnRenamed("circuitRef", "circuit_ref") \
+.withColumnRenamed("lat", "latitude") \
+.withColumnRenamed("lng", "longitude") \
+.withColumnRenamed("alt", "altitude") 
+
+# COMMAND ----------
+
+circuits_renamed_df.write.parquet("/mnt/group5dxc/silver/circuits")
---
+++ ingest_races_file.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+from pyspark.sql.functions import current_timestamp, to_timestamp, concat, col, lit
+
+# COMMAND ----------
+
+races_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
+                                  StructField("year", IntegerType(), True),
+                                  StructField("round", IntegerType(), True),
+                                  StructField("circuitId", IntegerType(), True),
+                                  StructField("name", StringType(), True),
+                                  StructField("date", DateType(), True),
+                                  StructField("time", StringType(), True),
+                                  StructField("url", StringType(), True) 
+])
+
+# COMMAND ----------
+
+races_df = spark.read \
+.option("header", True) \
+.schema(races_schema) \
+.csv("/mnt/group5dxc/bronze/races.csv")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import to_timestamp, concat, col, lit
+
+
+# COMMAND ----------
+
+races_with_timestamp_df = races_df.withColumn("race_timestamp", to_timestamp(concat(col('date'), lit(' '), col('time')), 'yyyy-MM-dd HH:mm:ss')) 
+
+# COMMAND ----------
+
+races_with_ingestion_date_df = races_with_timestamp_df.withColumn("ingestion_date", current_timestamp()) 
+
+# COMMAND ----------
+
+races_selected_df = races_with_ingestion_date_df.select(col('raceId').alias('race_id'), col('year').alias('race_year'), 
+                                                   col('round'), col('circuitId').alias('circuit_id'),col('name'), 
+                                                        col('ingestion_date'), col('race_timestamp'))
+
+# COMMAND ----------
+
+races_selected_df.write.mode('overwrite').partitionBy('race_year').parquet("/mnt/group5dxc/silver/races")
+
